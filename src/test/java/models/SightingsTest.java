787dd18cf387aaa19316f304a449b6d8ff40634b
Linus Mutugi <linusmutugi5178@gmail.com>
2020-05-01 20:19:38 +0300

    sightings instantiates correctly

--- src/test/java/models/SightingsTest.java
+++ src/test/java/models/SightingsTest.java
@@ -1,7 +1,22 @@
 package models;
 
+import org.junit.Rule;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class SightingsTest {
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
 
+    @Test
+    public void sightingsInstantiatesCorrectly_true() {
+    Sightings testSite=createSight();
+    assertEquals(true, testSite instanceof Sightings);
+    }
+
+public Sightings createSight()
+{
+    return new Sightings(1,"Near The River","James");
+}
 }
