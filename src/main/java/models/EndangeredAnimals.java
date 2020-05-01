4069295602276184ee195eefb7a673f553a97425
Linus Mutugi <linusmutugi5178@gmail.com>
2020-05-01 11:37:39 +0300

    create endangered animals class with constructor and override methods

---
+++ src/main/java/models/EndangeredAnimals.java
@@ -0,0 +1,40 @@
+package models;
+
+public class EndangeredAnimals extends Animals implements Animal{
+
+    public EndangeredAnimals(String name, String gender, String type) {
+        this.name=name;
+        this.gender=gender;
+        this.type=type;
+    }
+
+    @Override
+    public void save() {
+
+    }
+
+    @Override
+    public void findById() {
+
+    }
+
+    @Override
+    public void findAll() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void removeById() {
+
+    }
+
+    @Override
+    public void removeAll() {
+
+    }
+}
