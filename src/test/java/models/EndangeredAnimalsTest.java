14d376f101b4161f0f0fdc0e3e8f4f952a93c358
Linus Mutugi <linusmutugi5178@gmail.com>
2020-05-01 15:03:24 +0300

    create endangered animals test class

---
+++ src/test/java/models/EndangeredAnimalsTest.java
@@ -0,0 +1,11 @@
+package models;
+
+import org.junit.Rule;
+
+import static org.junit.Assert.*;
+
+public class EndangeredAnimalsTest {
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+}
