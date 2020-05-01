9fabb3c3d6367a162a40be3617d46c10812d5815
Linus Mutugi <linusmutugi5178@gmail.com>
2020-05-01 20:12:19 +0300

    constructor and getters created

--- src/main/java/models/Sightings.java
+++ src/main/java/models/Sightings.java
@@ -1,5 +1,30 @@
 package models;
 
 public class Sightings {
+    private int id;
+    private int animalId;
+    private String location;
+    private String rangerName;
 
+    public Sightings(int animalId, String location, String rangerName) {
+        this.animalId = animalId;
+        this.location = location;
+        this.rangerName = rangerName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAnimalId() {
+        return animalId;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getRangerName() {
+        return rangerName;
+    }
 }
