522837a7c9772de44b6dd24a76f0bd8a279c117d
Linus Mutugi <linusmutugi5178@gmail.com>
2020-05-01 11:09:43 +0300

    animal interface complete

---
+++ src/main/java/models/Animal.java
@@ -0,0 +1,17 @@
+package models;
+
+public interface Animal {
+//    CREATE ANIMAL
+    void save();
+
+//    READ ANIMAL
+    void findById();
+    void findAll();
+
+//    UPDATE ANIMAL
+    void update();
+
+//    DESTROY ANIMAL
+    void  removeById();
+    void removeAll();
+}
