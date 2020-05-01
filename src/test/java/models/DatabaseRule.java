48dc583d4f5409bc5f6349d5b342d2718d451092
Linus Mutugi <linusmutugi5178@gmail.com>
2020-05-01 12:26:49 +0300

    database test complete

--- src/test/java/models/DatabaseRule.java
+++ src/test/java/models/DatabaseRule.java
@@ -1,7 +1,23 @@
 package models;
 
 import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+import org.sql2o.Sql2o;
 
 public class DatabaseRule extends ExternalResource {
+    @Override
+    protected void before() {
+        Database.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test","wildlifeTracker", "wildlifeTracker");
+    }
 
+    @Override
+    protected void after(){
+        try (Connection conn=Database.sql2o.open())
+        {
+            String removeAnimalQuery="DELETE FROM animals";
+            conn.createQuery(removeAnimalQuery)
+                    .executeUpdate();
+        }
+
+    }
 }
