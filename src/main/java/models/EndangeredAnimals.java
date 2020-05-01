27b5455f39f1c71683b7e1d9661647f0e7584449
Linus Mutugi <linusmutugi5178@gmail.com>
2020-05-01 12:06:27 +0300

    constants added

--- src/main/java/models/EndangeredAnimals.java
+++ src/main/java/models/EndangeredAnimals.java
@@ -1,11 +1,29 @@
 package models;
 
 public class EndangeredAnimals extends Animals implements Animal{
+    private String age;
+    private String health;
+    public static final String AGE_NEWBORN = "newBorn";
+    public static final String AGE_YOUNG = "young";
+    public static final String AGE_ADULT = "adult";
 
-    public EndangeredAnimals(String name, String gender, String type) {
+    public static String HEALTH_HEALTHY = "healthy";
+    public static String HEALTH_ILL = "ill";
+    public static String HEALTH_OKAY = "okay";
+
+    public static String GENDER_MALE = "male";
+    public static String GENDER_FEMALE = "female";
+    public EndangeredAnimals(String name, String type) {
         this.name=name;
-        this.gender=gender;
         this.type=type;
+        gender=GENDER_MALE;
+        gender=GENDER_FEMALE;
+        age=AGE_NEWBORN;
+        age=AGE_YOUNG;
+        age=AGE_ADULT;
+        health=HEALTH_HEALTHY;
+        health=HEALTH_ILL;
+        health=HEALTH_OKAY;
     }
 
     @Override
