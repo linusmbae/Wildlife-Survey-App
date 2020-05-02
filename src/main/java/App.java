8bb78edb5643c298ddb950812cd43ca564dd57a3
Linus Mutugi <linusmutugi5178@gmail.com>
2020-05-02 23:03:19 +0300

    animal form complete

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,11 +14,28 @@ public class App {
 
 
         get("/",(request, response) ->
-{
-    Map<String, Object>model=new HashMap<String, Object>();
-    List<EndangeredAnimals> endangeredAnimals=EndangeredAnimals.getAll();
-    model.put("endangeredAnimals", endangeredAnimals);
-    return new ModelAndView(model, "index.hbs");
-}, new HandlebarsTemplateEngine());
+        {
+                Map<String, Object>model=new HashMap<String, Object>();
+                List<EndangeredAnimals> endangeredAnimals=EndangeredAnimals.getAll();
+                model.put("endangeredAnimals", endangeredAnimals);
+                return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/animals/new", (request, response) ->
+        {
+            Map<String, Object>model= new HashMap<String, Object>();
+            return new ModelAndView(model,"register_animals_form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/animals/new", (request, response) ->
+        {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String name = request.queryParams("name");
+            int rangerId = Integer.parseInt(request.queryParams("rangerId"));
+            EndangeredAnimals endangeredAnimals= new EndangeredAnimals(name,rangerId);
+            model.put("endangeredAnimals",endangeredAnimals);
+            response.redirect("/");
+            return null;
+        },new HandlebarsTemplateEngine());
     }
 }
