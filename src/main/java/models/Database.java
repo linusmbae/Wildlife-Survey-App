2ff30500189a8a4d415bcc59a8d1f15509068026
Linus Mutugi <linusmutugi5178@gmail.com>
2020-05-04 21:25:35 +0300

    changes

--- src/main/java/models/Database.java
+++ src/main/java/models/Database.java
@@ -1,6 +1,9 @@
 package models;
 import org.sql2o.*;
 public class Database {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "postgres", "12345");
+
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://aoizczhjwnjwso:4dd97d27fc56f55ec91001a254fc9be4e9205d50e84c4548c03036ca81bbee2d@ec2-52-202-22-140.compute-1.amazonaws.com:5432/d7jjinba7c9cui", "aoizczhjwnjwso", "4dd97d27fc56f55ec91001a254fc9be4e9205d50e84c4548c03036ca81bbee2d");
+
+//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "postgres", "12345");
 
 }
--- src/test/java/models/DatabaseRule.java
+++ src/test/java/models/DatabaseRule.java
@@ -7,7 +7,9 @@ import org.sql2o.Sql2o;
 public class DatabaseRule extends ExternalResource {
     @Override
     protected void before() {
-        Database.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "postgres", "12345");
+        Database.sql2o = new Sql2o("jdbc:postgresql://aoizczhjwnjwso:4dd97d27fc56f55ec91001a254fc9be4e9205d50e84c4548c03036ca81bbee2d@ec2-52-202-22-140.compute-1.amazonaws.com:5432/d7jjinba7c9cui", "aoizczhjwnjwso", "4dd97d27fc56f55ec91001a254fc9be4e9205d50e84c4548c03036ca81bbee2d");
+
+//        Database.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "postgres", "12345");
     }
 
     @Override
